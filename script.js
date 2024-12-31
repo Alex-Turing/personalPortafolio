21b334c5fd000566c6942175954751b3c97e6262
Alex-Turing <alexanderhernandez0684@gmail.com>
2024-12-30 23:12:11 -0500

    responsive design complete

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-document.querySelectorAll('.work-experience__card').forEach(card => {
-    card.addEventListener('click', () => {
-        card.classList.toggle('flipped');
-    });
+const projectCards = document.querySelectorAll('.projects__book-card');
+const workExperienceCards = document.querySelectorAll('.work-experience__card');
+
+workExperienceCards.forEach(card => {
+    card.addEventListener('click', toggleFlip);
+    card.addEventListener('touchstart', toggleFlip);
 });
+
+projectCards.forEach(card => {
+    card.addEventListener('click', toggleFlip);
+    card.addEventListener('touchstart', toggleFlip);
+});
+
+
+function toggleFlip(event) {
+    event.preventDefault(); 
+    this.classList.toggle('flipped');
+}
